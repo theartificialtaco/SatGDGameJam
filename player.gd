4dbec38a16651f2bef2b648348e54947c6906cbd
theartificialtaco <rashad.rchowdhury@gmail.com>
2022-06-25 21:11:40 +0600

    uwu tiles-chan is colliding
    ignore the name please, michaeel. thank you.

--- player.gd
+++ player.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
-
+onready var ray = $RayCast2D
+var gridsize = 16
 var inputs = {
 	'ui_up': Vector2.UP,
 	'ui_down': Vector2.DOWN,
@@ -14,4 +15,8 @@ func _unhandled_input(event):
 			move(dir)
 
 func move(dir):
-	position += inputs[dir] * 16
+	var vectorpos = inputs[dir] * gridsize
+	ray.cast_to = vectorpos
+	ray.force_raycast_update()
+	if !ray.is_colliding():
+		position += vectorpos
